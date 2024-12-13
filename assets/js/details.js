86cd72319dde5302aeb0ce9c770609852ad0a716
Manuel Palmieri <manuelpalm2002@gmail.com>
2024-12-13 17:53:43 +0100

    dimenticato letterina nella chiave di autorizzazione

--- assets/js/details.js
+++ assets/js/details.js
@@ -16,7 +16,7 @@ async function getProduct() {
     try {
         const response = await fetch("https://striveschool-api.herokuapp.com/api/product/" + id, {
             headers: {
-                "Authorization": "Bearer eJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAwNWQyMjA3MTAwMTVkZTJmNTQiLCJpYXQiOjE3MzQwODI1NjUsImV4cCI6MTczNTI5MjE2NX0.sAmcXBQzFYp9u11QqxZz32enq2KKxlIfWdxeXPlWDAw"
+                "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzVjMDAwNWQyMjA3MTAwMTVkZTJmNTQiLCJpYXQiOjE3MzQwODI1NjUsImV4cCI6MTczNTI5MjE2NX0.sAmcXBQzFYp9u11QqxZz32enq2KKxlIfWdxeXPlWDAw"
             }
         })
         const data = await response.json();
